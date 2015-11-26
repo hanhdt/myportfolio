0a2c2891f594820c927a3ee590f4fbfeec95f91f
Hanh D. TRAN <tranduchanh.ms@gmail.com>
2015-11-26 07:18:16 +0000

    creating and migrating carts, orders

---
+++ app/Order.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'orders';
+    
+    protected $fillable = array('member_id', 'address', 'total');
+    
+    public function orderItems(){
+        return $this->belongsToMany('Book')->withPivot('amount','total');
+    }
+}
